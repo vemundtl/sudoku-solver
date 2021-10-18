7f0e5a036597f8a7f0dc8290ba2e0f9b29c32e17
vemundtl <vemund@thallaug.org>
2021-10-18 19:15:02 +0200

    fikk mekka gui med litt fra internett

--- solver.py
+++ solver.py
@@ -1,4 +1,4 @@
-testboard = [
+board = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
     [0,0,0,6,0,1,0,7,8],
@@ -10,59 +10,70 @@ testboard = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-def print_board(board):
-    for i in range(len(board)):
-        if i % 3 == 0 and i != 0: 
-            print("--------------")
-        for j in range(len(board[0])):
-            if j%3 == 0 and j != 0: 
-                print(" | ", end="")
-                
-            if j== 8: 
-                print(board[i][j])
-            else: 
-                print(str(board[i][j])+" ", end="")
 
-def find_empty_square(board): 
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == 0:
-                return (i,j)
-    return None
-
-def solve_board(board): 
-    empty_square = find_empty_square(board)
-    if not empty_square:
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
         return True
-    else: 
-        row, col = empty_square
-        for i in range(1,10):
-            if check_if_valid(i, (row,col), board):
-                board[row][col] = i
+    else:
+        row, col = find
 
-                if solve_board(board):
-                    return True
-                board[row][col] = 0
-        return False
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
 
-def check_if_valid(num, pos, board): 
-    for i in range(len(board[0])):
-        if board[pos[0]][i] == num and pos[1] != i:
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
             return False
 
-    for i in range(len(board)):
-        if board[pos[1]][i] == num and pos[0] != i:
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
             return False
 
-    x = pos[1] //  3
-    y = pos[0] // 3 
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
 
-    for i in range(y*3,y*3+3):
-        for j in range(x*3,x*3+3):
-            if board[i][j] == num and (i,j) != pos:
-                return False    
-    
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
     return True
 
 
-print(find_empty_square(testboard))
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
+
+print_board(board)
+solve(board)
+print("------------")
+print_board(board)
